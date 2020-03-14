0d80242a5df70c32224b7348145723ddbfc2fb1d
Muzafar Ahmed <ahmed.muzafar@gmail.com>
2020-03-14 20:24:31 +0100

    Initial commit

---
+++ lib/muzafar_palindrome.rb
@@ -0,0 +1,6 @@
+require "muzafar_palindrome/version"
+
+module MuzafarPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/muzafar_palindrome_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class MuzafarPalindromeTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::MuzafarPalindrome::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
