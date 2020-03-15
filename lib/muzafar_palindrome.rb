1170a7df0e374f41f4e3269b910678898591fbd2
Muzafar Ahmed <ahmed.muzafar@gmail.com>
2020-03-15 10:02:34 +0100

    Finish working and refactored palindrome method

--- lib/muzafar_palindrome.rb
+++ lib/muzafar_palindrome.rb
@@ -1,6 +1,18 @@
 require "muzafar_palindrome/version"
 
-module MuzafarPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+# Creating a module with functionality
+class String
+
+    # Returns true for a palindrome. False otherwise
+    def palindrome?
+      processed_content == processed_content.reverse
+    end
+
+    # Keeps the below functions private
+    private
+
+      # Processes content for a palindrome detection.
+      def processed_content
+        scan(/[a-z]/i).join.downcase
+      end
 end
